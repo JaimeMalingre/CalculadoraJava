cd6d422125ebfd03ff42bbafc312c0459668ce28
Jaime Malingre <jaimemalingre@gmail.com>
2023-11-02 22:48:50 +0100

    Calculadora

--- app/src/main/java/com/example/calculadorajava/MainActivity.java
+++ app/src/main/java/com/example/calculadorajava/MainActivity.java
@@ -25,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView NameBoton = findViewById(R.id.btnIgual);
-        igualBtn = findViewById(R.id.btnIgual);
+
 
         //Botones de numero
 
